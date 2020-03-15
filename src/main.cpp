6d5c50d1b975dc380a215eb563a91fccad67fd11
David Boucher <dboucher@centreon.com>
2020-03-15 21:44:26 +0100

    enh(debug): Debug functions is available but not enabled by default.

--- src/main.cpp
+++ src/main.cpp
@@ -82,8 +82,8 @@ GLuint load_shaders(const std::string &vertex_shader,
     exit(2);
   }
 
-  //glDeleteShader(vertexShaderObject);
-  //glDeleteShader(fragmentShaderObject);
+  glDeleteShader(vertexShaderObject);
+  glDeleteShader(fragmentShaderObject);
   return program;
 }
 
@@ -99,76 +99,76 @@ void APIENTRY glDebugOutput(unsigned int source, unsigned int type,
 
   std::cout << "---------------" << std::endl;
   std::cout << "Debug message (" << id << "): " << message << std::endl;
-//
-//  switch (source) {
-//  case GL_DEBUG_SOURCE_API:
-//    std::cout << "Source: API";
-//    break;
-//  case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-//    std::cout << "Source: Window System";
-//    break;
-//  case GL_DEBUG_SOURCE_SHADER_COMPILER:
-//    std::cout << "Source: Shader Compiler";
-//    break;
-//  case GL_DEBUG_SOURCE_THIRD_PARTY:
-//    std::cout << "Source: Third Party";
-//    break;
-//  case GL_DEBUG_SOURCE_APPLICATION:
-//    std::cout << "Source: Application";
-//    break;
-//  case GL_DEBUG_SOURCE_OTHER:
-//    std::cout << "Source: Other";
-//    break;
-//  }
-//  std::cout << std::endl;
-//
-//  switch (type) {
-//  case GL_DEBUG_TYPE_ERROR:
-//    std::cout << "Type: Error";
-//    break;
-//  case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-//    std::cout << "Type: Deprecated Behaviour";
-//    break;
-//  case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-//    std::cout << "Type: Undefined Behaviour";
-//    break;
-//  case GL_DEBUG_TYPE_PORTABILITY:
-//    std::cout << "Type: Portability";
-//    break;
-//  case GL_DEBUG_TYPE_PERFORMANCE:
-//    std::cout << "Type: Performance";
-//    break;
-//  case GL_DEBUG_TYPE_MARKER:
-//    std::cout << "Type: Marker";
-//    break;
-//  case GL_DEBUG_TYPE_PUSH_GROUP:
-//    std::cout << "Type: Push Group";
-//    break;
-//  case GL_DEBUG_TYPE_POP_GROUP:
-//    std::cout << "Type: Pop Group";
-//    break;
-//  case GL_DEBUG_TYPE_OTHER:
-//    std::cout << "Type: Other";
-//    break;
-//  }
-//  std::cout << std::endl;
-//
-//  switch (severity) {
-//  case GL_DEBUG_SEVERITY_HIGH:
-//    std::cout << "Severity: high";
-//    break;
-//  case GL_DEBUG_SEVERITY_MEDIUM:
-//    std::cout << "Severity: medium";
-//    break;
-//  case GL_DEBUG_SEVERITY_LOW:
-//    std::cout << "Severity: low";
-//    break;
-//  case GL_DEBUG_SEVERITY_NOTIFICATION:
-//    std::cout << "Severity: notification";
-//    break;
-//  }
-//  std::cout << std::endl;
-//  std::cout << std::endl;
+
+  switch (source) {
+  case GL_DEBUG_SOURCE_API:
+    std::cout << "Source: API";
+    break;
+  case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+    std::cout << "Source: Window System";
+    break;
+  case GL_DEBUG_SOURCE_SHADER_COMPILER:
+    std::cout << "Source: Shader Compiler";
+    break;
+  case GL_DEBUG_SOURCE_THIRD_PARTY:
+    std::cout << "Source: Third Party";
+    break;
+  case GL_DEBUG_SOURCE_APPLICATION:
+    std::cout << "Source: Application";
+    break;
+  case GL_DEBUG_SOURCE_OTHER:
+    std::cout << "Source: Other";
+    break;
+  }
+  std::cout << std::endl;
+
+  switch (type) {
+  case GL_DEBUG_TYPE_ERROR:
+    std::cout << "Type: Error";
+    break;
+  case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+    std::cout << "Type: Deprecated Behaviour";
+    break;
+  case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+    std::cout << "Type: Undefined Behaviour";
+    break;
+  case GL_DEBUG_TYPE_PORTABILITY:
+    std::cout << "Type: Portability";
+    break;
+  case GL_DEBUG_TYPE_PERFORMANCE:
+    std::cout << "Type: Performance";
+    break;
+  case GL_DEBUG_TYPE_MARKER:
+    std::cout << "Type: Marker";
+    break;
+  case GL_DEBUG_TYPE_PUSH_GROUP:
+    std::cout << "Type: Push Group";
+    break;
+  case GL_DEBUG_TYPE_POP_GROUP:
+    std::cout << "Type: Pop Group";
+    break;
+  case GL_DEBUG_TYPE_OTHER:
+    std::cout << "Type: Other";
+    break;
+  }
+  std::cout << std::endl;
+
+  switch (severity) {
+  case GL_DEBUG_SEVERITY_HIGH:
+    std::cout << "Severity: high";
+    break;
+  case GL_DEBUG_SEVERITY_MEDIUM:
+    std::cout << "Severity: medium";
+    break;
+  case GL_DEBUG_SEVERITY_LOW:
+    std::cout << "Severity: low";
+    break;
+  case GL_DEBUG_SEVERITY_NOTIFICATION:
+    std::cout << "Severity: notification";
+    break;
+  }
+  std::cout << std::endl;
+  std::cout << std::endl;
 }
 
 int main(void) {
@@ -185,7 +185,8 @@ int main(void) {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,
                  GL_TRUE); // To make MacOS happy; should not be needed
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+  // FIXME DBR: To enable debugging.
+  //glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
   GLint flags;
   glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
